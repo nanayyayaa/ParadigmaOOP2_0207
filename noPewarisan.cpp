655820e3171766d980200b793642ef139bb00e3a
nanayyayaa <jiejueejuju@gmail.com>
2026-05-18 18:16:07 +0700

    commit 17

---
+++ noPewarisan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final{
+public:
+    virtual void perkenalan() {
+        cout << "Halo saya Function dari base class";
+    }
+};
+class baseClass1 {
+public:
+    virtual void perkenalan() {
+        cout << "Halo saya Function dari base class";
+    }
+};
+class derivedClass : public baseClass1 {
+public:
+}
