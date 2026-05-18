b7bf0b3980a339a6f85dfa0b9133bc3fb19f0ed0
nanayyayaa <jiejueejuju@gmail.com>
2026-05-18 17:58:52 +0700

    commit 10

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -11,4 +11,14 @@ public:
         x = a;
         y = b;
     }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
+int main() {
+    AbstraksiKlas ak;
 }
+
