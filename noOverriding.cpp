a1df3214061fdacdcb44a05233437a8724c4b99f
nanayyayaa <jiejueejuju@gmail.com>
2026-05-18 17:40:24 +0700

    commit 5

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -7,3 +7,15 @@ public:
         cout << "Halo saya Function dari base class";
     }
 };
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Halo saya Function dari derived Class";
+    }
+};
+int main() {
+    derivedClass a;
+    a.perkenalan1();
+
+    return 0;
+}
